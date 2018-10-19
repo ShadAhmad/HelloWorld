f72debb75606c6349ef6fd5703b0576b7eb02f59
Shad Ahmad Zaidi <shadzaidi123@gmail.com>
2018-10-19 16:38:09 +0530

    Removed unnecessary code
    The utility & io classes of java are not required for a simple program to print Hello World

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,7 @@
-import java.util.*;
-import java.io.*;
+//import java.util.*;
+//import java.io.*;
+
+//The code above isn't required as long as we only need to print data on screen
 
 class HelloWorld {
   public static void main(String[] args) {
